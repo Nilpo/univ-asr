db1c745a15fccbcdd421eb9a5f6bfdd38ca35864
Helder Correia <heldercorreia@morfose.net>
2013-05-30 17:12:11 +0000

    Use the dmz's public address for external requests to the DNS.

--- cookbooks/dns/recipes/default.rb
+++ cookbooks/dns/recipes/default.rb
@@ -41,6 +41,24 @@ end
   end
 end
 
+# Listen on our public IP for external requests
+bash "external" do
+  user "root"
+  code <<-EOH
+    # IP manipulation
+    PUBLIC_IP=`ifconfig eth2 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1 }'`
+    PUBLIC_NET=`echo $PUBLIC_IP | cut -d. -f1-3`
+    HOST_PART=`echo $PUBLIC_IP | cut -d. -f4`
+    REV=`echo $PUBLIC_NET | awk -F. '{s="";for (i=NF;i>1;i--) s=s sprintf("%s.",$i);$0=s $1}1'`
+
+    # Update named.conf and zone files
+    sed -i~ "s/\\(listen-on port 53\\).*/\\1   { $PUBLIC_IP; 172.31.0.1; 127.0.0.1; };/1g; /DYNAMIC/ { s/0.31.172/$REV/}" /etc/named.conf
+    sed -i~ "s/172.31.0.1/$PUBLIC_IP/" /var/named/external/imbcc.pt.db
+    sed -i~ "s/^1\\(.*PTR.*\\)/$HOST_PART\\1/g" /var/named/external/0.31.172.rev
+    mv /var/named/external/0.31.172.rev /var/named/external/$REV.rev
+  EOH
+end
+
 service "named" do
   supports :reload => true, :status => true
   action [ :enable, :start ]
